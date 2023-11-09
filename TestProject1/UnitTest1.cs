a14237cdfa55812df27ac99e1834253c95cdda12
KanyakornPuengmon <144195697+KanyakornPuengmon@users.noreply.github.com>
2023-11-09 19:38:13 +0700

    Update UnitTest1.cs

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,5 +1,5 @@
-using Project_65030018;
-namespace TestProject1
+using project_65030018;
+namespace TestProject
 {
     [TestClass]
     public class UnitTest1
@@ -8,6 +8,14 @@ namespace TestProject1
         public void TestProjectMembersCount()
         {
             Assert.IsTrue(Enum.GetNames(typeof(Project.Members)).Length == 4);
+        }
+        [TestMethod]
+        public void TestProjectMemberIds()
+        {
+            Assert.IsTrue((int)Project.Members.Pear == 1);
+            Assert.IsTrue((int)Project.Members.Ming == 2);
+            Assert.IsTrue((int)Project.Members.Mark == 3);
+            Assert.IsTrue((int)Project.Members.Bob == 4);
         }
     }
 }
