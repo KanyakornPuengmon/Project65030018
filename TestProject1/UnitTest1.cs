ac20c4282f29aaa20b4643aa6e367ee70cafe0d3
KanyakornPuengmon <144195697+KanyakornPuengmon@users.noreply.github.com>
2023-11-13 15:52:44 +0700

    Update UnitTest1.cs

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -4,11 +4,7 @@ namespace TestProject
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestProjectMembersCount()
-        {
-            Assert.IsTrue(Enum.GetNames(typeof(Project.Members)).Length == 4);
-        }
+    
         [TestMethod]
         public void TestProjectMemberIds()
         {
